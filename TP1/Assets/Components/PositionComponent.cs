2f0fe5dcc55edc71ac785daa6b1dded3b7411eff
IceInMyDrink <matis_gregoire@hotmail.com>
2024-01-17 17:45:53 -0500

    MG: Created components

---
+++ TP1/Assets/Components/PositionComponent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PositionComponent : IComponent
+{
+    Vector2 position;
+}
---
+++ TP1/Assets/Components/SpeedComponent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SpeedComponent : IComponent
+{
+    Vector2 speed;
+}
